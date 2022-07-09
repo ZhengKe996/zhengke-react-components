90744f3359175df97bde4285afe4f6238f1b2b8a
ZhengKe <3119531802@qq.com>
2022-07-09 10:20:39 +0800

    feat:  清理文件目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button, { ButtonSize, ButtonType } from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className="123  " btnType={ButtonType.Primary}>
+        Hello
+      </Button>
+      <Button btnType={ButtonType.Default}>Hello</Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com">
+        Link
+      </Button>
+      <Button btnType={ButtonType.Danger}>Hello</Button>
+      <hr />
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Hello
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        Hello
+      </Button>
+      <hr />
+      <Button btnType={ButtonType.Default} disabled={true}>
+        Hello
+      </Button>
+      <Button btnType={ButtonType.Link} disabled={true}>
+        Hello
+      </Button>
     </div>
   );
 }
